ee4d25df8cdad635e99f1544de927f6a22fc5509
Martin Urban <martin.urban@studmail.w-hs.de>
2025-08-02 12:42:18 +0200

    Add copyright notices and update module name in project files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
+/**
+ * Copyright (c) 2025, Martin Urban
+ * Licensed under the BSD 3-Clause License (https://opensource.org/license/bsd-3-clause)
+ * This file may not be copied, modified, or distributed except according to those terms.
+ * ======================================================================================
+ */
 package org.example;
 
+/**
+ * This class serves as the entry point for the application.
+ *
+ * @author Martin Urban
+ */
 public class Main {
 
     /**
